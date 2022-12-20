f42faec51a58cf6dd16253aa2c0ec9ef6338a332
Demon-IV <demiancnochaert@gmail.com>
2022-12-20 16:12:41 -0300

    first commit

---
+++ src/components/TaskCard.jsx
@@ -0,0 +1,20 @@
+import { useContext } from "react";
+import TaskContext from "../context/TaskContext";
+
+function TaskCard({ task }) {
+  /*const handleClick = (e) => {
+    e.preventDefault();
+    removeTask(task.id);
+  };*/
+  const { removeTask } = useContext(TaskContext);
+
+  return (
+    <div className ="bg-gray-800 text-white p-4 rounded-md">
+      <h1 className ="text-xl font-bold capitalize">{task.title}</h1>
+      <p className = "text-gray-400 text-sm">{task.description}</p>
+      <button className = "bg-red-800 px-2 py-1 hover:bg-red-500 rounded-md" onClick={() => removeTask(task.id)}>Eliminar</button>
+    </div>
+  );
+}
+
+export default TaskCard;
